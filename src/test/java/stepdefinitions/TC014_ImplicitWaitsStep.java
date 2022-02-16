a8b1644a584c6f32583a083f9bfb372b272ce895
Charlie Gilliland <Charlie.Gilliland@QUALITEST.LOCAL>
2022-02-16 11:12:30 +0000

    Adding ExplicitWaits test and updating ImplicitWaits test

--- src/test/java/stepdefinitions/TC014_ImplicitWaitsStep.java
+++ src/test/java/stepdefinitions/TC014_ImplicitWaitsStep.java
@@ -3,15 +3,17 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 public class TC014_ImplicitWaitsStep {
     WebDriver driver;
-    String url = "https://www.booking.com/index.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4Apmhs5AGwAIB0gIkYzBiMjMzNzktZjQzZC00OGQ0LTk3YTAtOTE1YTdiMzEwODg12AIE4AIB;sid=553d5718fbca361785e38d718cb65700;keep_landing=1&sb_price_type=total&";
+    String url = "https://the-internet.herokuapp.com/dynamic_loading/2";
 
     @Given("I have supplied the correct setup to add an implicit wait")
     public void i_have_supplied_the_correct_setup_to_add_an_implicit_wait(){
@@ -26,15 +28,20 @@ public class TC014_ImplicitWaitsStep {
     @When("I navigate to the web page for TC014")
     public void i_have_navigated_to_the_web_page_for_tc014(){
         // This will load the web page but not all elements will be visible once the page is loaded
-        driver.get(url);
+        driver.navigate().to(url);
     }
 
     @Then("The driver will implicitly wait before the test fails")
     public void the_driver_will_implicitly_wait_before_the_test_fails(){
-        // This pop-up will take longer to display on the screen
-        // As we have specified an implicit wait, selenium will wait the given amount of time before it fails this step
-        // To test, comment out line 23 and re-run the test
-        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        // Clicking this element will load another after a given amount of time
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+
+        // Because of the implicit wait, selenium will wait the given time before failing this step
+        WebElement loadedElement = driver.findElement(By.xpath("//h4[text()='Hello World!']"));
+
+        // We can then assert that the element was loaded successfully
+        Assert.assertTrue(loadedElement != null);
+
     }
 
 
